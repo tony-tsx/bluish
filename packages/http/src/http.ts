96ad95590766c64e31cfca72f7c816a62f130c4c
Tony <tony.js@zoho.eu>
2024-07-24 18:29:30 -0300

    feat(http): adds http setup

--- packages/http/src/http.ts
+++ packages/http/src/http.ts
@@ -42,31 +42,93 @@ export {
   ToAcceptReturn,
 } from './tools/action-helpers.js'
 
-import { Injectable, Middleware } from '@bluish/core'
+import { Injectable, Middleware, Setup } from '@bluish/core'
 import { HttpContext } from './models/HttpContext.js'
 import {
   BadRequest,
   HttpError,
+  NotAcceptable,
   UnsupportedMediaType,
 } from './errors/HttpError.js'
 import { Request } from './models/Request.js'
-import { toAccept } from './tools/action-helpers.js'
+import { toAccept, toContentTypes } from './tools/action-helpers.js'
 import { is } from 'type-is'
+import { MiddlewareRegistryLayer } from '../../core/dist/esm/models/Middleware.js'
+import { json } from './middlewares/body-parsers.js'
 
 Injectable(Request, {
   scope: 'request',
   resolve: context => context.request,
 })
 
-Middleware.register(HttpContext, async function http(context, next) {
-  context.response.headers['X-Powered-By'] = `Node ${process.version} / Bluish`
+Middleware.register(
+  MiddlewareRegistryLayer.Application,
+  HttpContext,
+  (context, next) => {
+    context.response.headers['X-Powered-By'] =
+      `Node ${process.version} / Bluish`
 
-  const accept = toAccept(context.runner.action)
+    return next()
+  },
+)
 
-  context.response.headers['Accept'] = accept.formats
+Middleware.register(
+  [MiddlewareRegistryLayer.Application, MiddlewareRegistryLayer.Controller],
+  HttpContext,
+  async (context, next) => {
+    try {
+      return await next()
+    } catch (error) {
+      if (typeof error !== 'object') throw error
 
-  try {
-    if (accept.all) return await next()
+      if (!(error instanceof HttpError)) throw error
+
+      context.response.status = error.status
+      context.response.headers['Content-Type'] = 'application/json'
+      context.response.body ??= JSON.stringify({
+        error: error.name,
+        message: error.message,
+      })
+    }
+  },
+)
+
+Middleware.register(
+  MiddlewareRegistryLayer.Application,
+  HttpContext,
+  (context, next) => {
+    if (!context.request.headers.accept) return next()
+
+    const contentTypes = toContentTypes(context.runner.action)
+
+    if (!contentTypes.length) return next()
+
+    const accepts = context.request.headers.accept
+      .split(',')
+      .map(type => type.trim().split(';')[0])
+
+    const has = contentTypes.some(contentType =>
+      accepts.some(accept => is(contentType.type, accept)),
+    )
+
+    if (!has)
+      throw new NotAcceptable(
+        'The requested content type is not available. Supported content types are: application/json, application/xml.',
+      )
+
+    return next()
+  },
+)
+
+Middleware.register(
+  MiddlewareRegistryLayer.Application,
+  HttpContext,
+  (context, next) => {
+    const accept = toAccept(context.runner.action)
+
+    context.response.headers['Accept'] = accept.formats
+
+    if (accept.all) return next()
 
     if (!context.request.headers['content-type'])
       throw new BadRequest('Content-Type header is missing')
@@ -74,17 +136,28 @@ Middleware.register(HttpContext, async function http(context, next) {
     if (!is(context.request.headers['content-type'], accept.formats))
       throw new UnsupportedMediaType('Unsupported media type')
 
-    return await next()
-  } catch (error) {
-    if (typeof error !== 'object') throw error
+    return next()
+  },
+)
 
-    if (!(error instanceof HttpError)) throw error
+export interface HttpSettings {
+  json?: boolean
+}
 
-    context.response.status = error.status
-    context.response.headers['Content-Type'] = 'application/json'
-    context.response.body ??= JSON.stringify({
-      error: error.name,
-      message: error.message,
-    })
-  }
-})
+export default function http(settings: HttpSettings) {
+  return new Setup({
+    onAction(action) {
+      if (settings.json) action.middlewares.unshift(json())
+    },
+    onController(controller) {
+      if (settings.json) {
+        controller.metadata['@http:content-type'] ??= []
+
+        controller.metadata['@http:content-type'].push({
+          type: 'application/json',
+          to: payload => JSON.stringify(payload),
+        })
+      }
+    },
+  })
+}
